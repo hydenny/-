0b31e7b3f8d44d21e1136a4120df35fae9b078bc
hayoung0402 <hy950402@gmail.com>
2021-04-11 03:30:20 +0900

    Update 미로 탈출.py

--- 이것이 코딩 테스트다/DFS&BFS/미로 탈출.py
+++ 이것이 코딩 테스트다/DFS&BFS/미로 탈출.py
@@ -1,37 +1,34 @@
-import sys
 from collections import deque
 
-n, m = map(int, sys.stdin.readline().split())
-
-graph = []
+n,m = map(int,input().split())
+board =[]
+dx = [0,1,0,-1]
+dy = [-1,0,+1,0]
 
 for i in range(n):
-    graph.append(list(map(int, sys.stdin.readline().rstrip())))
+    board.append(list(map(int,input())))
+visited = board
+def bfs(y,x):
+    cnt = 1
+    global visited, board
+    visited[y][x]= 0
+    q = deque()
+    q.append((y,x))
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-
-            if graph[nx][ny] == 0:
-                continue
-            
-            if graph[nx][ny] == 1:
-                graph[nx][ny] == graph[x][y] + 1
-                queue.append((nx, ny))
-
-    return graph[n - 1][m - 1]
-
-print(bfs(0, 0))
+    while 1:
+        (y,x) = q.popleft()
+        cnt+=1
+        count=0
+        for j in range(4):
+            tx = x + dx[j]
+            ty = y + dy[j]
+            if tx>=0 and tx<m and ty>=0 and ty<n and visited[ty][tx]==1:
+                if ty == n-1 and tx == m-1:
+                    return cnt
+                visited[ty][tx] = 0
+                q.append((ty,tx))
+                count+=1
+        if count==0:
+            cnt-=1
+                
+print(bfs(0,0))
