8b6d9e8dfcf45c9ae9d4e0fbd0c4dffc23154e4b
hayoung0402 <hy950402@gmail.com>
2021-04-13 02:27:44 +0900

    Update 미로 탈출.py

--- 이것이 코딩 테스트다/DFS&BFS/미로 탈출.py
+++ 이것이 코딩 테스트다/DFS&BFS/미로 탈출.py
@@ -1,12 +1,13 @@
 from collections import deque
+import sys
 
-n,m = map(int,input().split())
+n,m = map(int,sys.stdin.readline().rstrip().split())
 board =[]
 dx = [0,1,0,-1]
 dy = [-1,0,+1,0]
 
 for i in range(n):
-    board.append(list(map(int,input())))
+    board.append(list(map(int,sys.stdin.readline().rstrip())))
 visited = board
 def bfs(y,x):
     cnt = 1
