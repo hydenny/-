f709b3ba3d939f70b0bac9846799d9306d5845e4
hayoung0402 <hy950402@gmail.com>
2021-05-20 21:10:59 +0900

    Update 벌집.py

--- 백준 코딩테스트 문제풀이/기본 수학1/벌집.py
+++ 백준 코딩테스트 문제풀이/기본 수학1/벌집.py
@@ -1,15 +1,7 @@
 n = int(input())
+floor = 0
 
-if n == 1:
-    print(1)
-else:
-    r = n - 1
-    
-    floor = 1
-
-    while r > 6 * floor:
-        r -= 6 * floor
-        floor += 1
-
+while n - 1 > 3 * floor * (floor + 1):
+    floor += 1
 
 print(floor + 1)
