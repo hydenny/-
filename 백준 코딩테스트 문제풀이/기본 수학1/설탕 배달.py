77b962d9bbd21d6dcb9e6c29917dfa263bb151b8
hayoung0402 <hy950402@gmail.com>
2021-08-10 02:38:03 +0900

    1

--- 백준 코딩테스트 문제풀이/기본 수학1/설탕 배달.py
+++ 백준 코딩테스트 문제풀이/기본 수학1/설탕 배달.py
@@ -4,24 +4,18 @@ n = int(sys.stdin.readline().rstrip())
 
 three, five = 0, 0
 
-while n:
-    if n % 3 == 0:
-        n -= 3
-        three += 1
-
-    elif n % 5 == 0:
-        n -= 5
-        five += 1
-
-    else:
-        break
-
-if n != 0:
+if n == 4 or n == 7:
     print(-1)
+    quit()
 
-else:
-    if three >= 5:
-        five += 3 * three // 5
-        three = three % 5
+while n % 5:
+    n -= 3
+    three += 1
 
-    print(three + five)
+five = n // 5
+
+if three % 5 == 0:
+    five += three // 5
+    three -= three // 5 * 3
+
+print(three + five)
