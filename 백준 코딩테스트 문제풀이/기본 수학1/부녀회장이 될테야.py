016d6c7d750b5b7f761dda734b7d458e258e7a5b
hayoung0402 <hy950402@gmail.com>
2021-08-10 01:02:52 +0900

    1

--- 백준 코딩테스트 문제풀이/기본 수학1/부녀회장이 될테야.py
+++ 백준 코딩테스트 문제풀이/기본 수학1/부녀회장이 될테야.py
@@ -2,12 +2,25 @@ import sys
 
 t = int(sys.stdin.readline().rstrip())
 
-dp = [[0] * 270]
+dp = [[0] * 15 for i in range(15)]
 
-for i in range()
+def living_number(k, n):
+    if dp[k][n] == 0:
+        if k == 0:
+            dp[k][n] = n
+            return n
+        
+        else:
+            sum = 0
+            for i in range(1, n + 1):
+                sum += living_number(k - 1, i)
+            dp[k][n] = sum
+            return sum
+
+    else:
+        return dp[k][n]
 
 for i in range(t):
     k = int(sys.stdin.readline().rstrip())
     n = int(sys.stdin.readline().rstrip())
-
-    print(living(k, n))
+    print(living_number(k, n))
