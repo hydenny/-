e0fc47d0dbcd3d20f18bcb7cc2db185b55aae0bc
hayoung0402 <hy950402@gmail.com>
2021-09-18 19:49:07 +0900

    1

---
+++ 백준 코딩테스트 문제풀이/재귀/팩토리얼.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    result = 1
+    if n > 0:
+        result = n * factorial(n - 1)
+
+    return result
+    
+n = int(input())
+
+print(factorial(n))
