4d57034fcff68a76ee04754ed464331ac7c89623
hayoung0402 <hy950402@gmail.com>
2021-09-30 03:44:36 +0900

    1

--- 백준 코딩테스트 문제풀이/Rise/그리디/수리공 항승.py
+++ 백준 코딩테스트 문제풀이/Rise/그리디/수리공 항승.py
@@ -8,22 +8,20 @@ start = 0
 end = start + 1
 
 count = 0
-
+if n == 1:
+    print(1)
+    exit()
 while end != n:    
-    #print(start, end, end=(" "))
     d = array[end] - array[start] + 1
     if d > l:
-        if start == 0:
-            count += 1
-            start += 1
-            end += 1
-        else:
-            count += 1
-            start = end
-            end = start + 1
+        count += 1
+        start = end
+        end = start + 1
     else:
         if end == n - 1:
             count += 1
         end += 1
+    if start == n - 1:
+        count += 1
 
 print(count)
