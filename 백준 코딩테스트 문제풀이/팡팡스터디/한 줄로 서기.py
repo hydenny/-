3571554f2e50a7b7bac0e97a273b6ec67922d52b
hayoung0402 <hy950402@gmail.com>
2021-10-30 03:15:39 +0900

    한 줄로 서기

--- 백준 코딩테스트 문제풀이/팡팡스터디/한 줄로 서기.py
+++ 백준 코딩테스트 문제풀이/팡팡스터디/한 줄로 서기.py
@@ -2,7 +2,23 @@ n = int(input())
 
 numbers = list(map(int, input().split()))
 
-array = [] * n
+array = [100] * n
+for i in range(n):
+    if i == 0:
+        array[numbers[i]] = i + 1
+        continue
 
-for i in numbers:
-    
+    count = numbers[i]
+
+    for j in range(n):
+        if array[j] == 100:
+            if count == 0:
+                array[j] = i + 1
+                break
+            else:
+                count -= 1
+
+        else:
+            continue
+
+print(' '.join(map(str, array)))
