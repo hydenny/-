0585db6e66db6114106561b711c1feba4686a24a
hayoung0402 <hy950402@gmail.com>
2021-11-16 17:09:43 +0900

    팔

--- 백준 코딩테스트 문제풀이/팡팡스터디/팔.py
+++ 백준 코딩테스트 문제풀이/팡팡스터디/팔.py
@@ -1,16 +1,19 @@
 l, r = map(str, input().split())
 
-if l == r or len(l) == 1:
+if len(l) != len(r):
+    print(0)
+    
+elif l == r:
     print(l.count('8'))
     
-else:
+else:   #l r 서로 다른 값이면서 자릿수 같은 경우
     idx = 0
     count = 0
     while idx < len(l):
-        if l[idx] == 8 or r[idx] == 8:
-            if l[idx] != r[idx]:
-                print(count)
-                exit()
-        count += 1
-        idx += 1
+        if l[idx] == r[idx]:
+            if l[idx] == '8':
+                count += 1
+            idx += 1
+        else:
+            break
     print(count)
