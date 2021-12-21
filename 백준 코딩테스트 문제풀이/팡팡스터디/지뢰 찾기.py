76cc86caaa8b989bfa8f31ec3e0aecbd9a6faab2
hayoung0402 <hy950402@gmail.com>
2021-12-22 01:07:14 +0900

    완료

--- 백준 코딩테스트 문제풀이/팡팡스터디/지뢰 찾기.py
+++ 백준 코딩테스트 문제풀이/팡팡스터디/지뢰 찾기.py
@@ -2,39 +2,52 @@ import sys
 
 
 def check_zero(x, y):
-    #상,하,좌,우,왼아래,왼위,오른아래,오른위
-    dx = [0, 0, -1, 1, -1, -1, 1, 1]
-    dy = [-1, 1, 0, 0, 1, -1, 1, -1]
-    
-    if board[y][x] == '0':
-        for i in range(8):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 < nx < n - 1 and 0 < ny < n - 1:
-                flag_board[ny][nx] = False
-                
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or ny < 0 or nx >= n or ny >= n:
+            continue
+        if board[nx][ny] == '0':
+            return False
+    return True               
+
+
+def board_minus(x, y):
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or ny < 0 or nx >= n or ny >= n or board[nx][ny] == '#':
+            continue
+        board[nx][ny] = str(int(board[nx][ny]) - 1)
 
 def solution(n):
-    if n <= 2:
-        print(0)
-        return
-    for y in range(n):
-        for x in range(n):
-            if y == 0 or y == n - 1 or x == 0 or x == n - 1:
-                check_zero(x, y)
-    zeros = 0
-    for i in range(n):
-        zeros += flag_board[i].count(False)
-    for i in range(n):
-        print(flag_board[i])    
-    print((n - 2) ** 2 - zeros)
+    if n <= 4:
+        count = 0
+    else:
+        count = (n - 4) ** 2
+    for i in range(1, n-1):
+        if check_zero(1, i):
+            count += 1
+            board_minus(1, i)
+        if check_zero(i, n-2):
+            count += 1
+            board_minus(i, n-2)
+        if check_zero(n-2, i):
+            count += 1
+            board_minus(n-2, i)
+        if check_zero(i, 1):
+            count += 1
+            board_minus(i, 1)
+    print(count)
 
 
 n = int(input())
 board = []
 for _ in range(n):
     board.append(list(map(str, input().rstrip())))
-  
-flag_board = [[True] * n for _ in range(n)]
+
+#상,하,좌,우,왼아래,왼위,오른아래,오른위
+dx = [0, 0, -1, 1, -1, -1, 1, 1]
+dy = [-1, 1, 0, 0, 1, -1, 1, -1]
 
 solution(n)
