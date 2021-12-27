2d989fe224e3c1f22cc2c3f0f27b295b00c6273e
hayoung0402 <hy950402@gmail.com>
2021-12-27 23:54:53 +0900

    후보 추천

--- 백준 코딩테스트 문제풀이/팡팡스터디/후보 추천하기.py
+++ 백준 코딩테스트 문제풀이/팡팡스터디/후보 추천하기.py
@@ -4,3 +4,8 @@ n = int(input())
 recommend = int(input())
 number = list(map(int, input().split()))
 
+sets = set(number[-(n + 1):])
+sets = list(sets)
+sets.sort()
+for i in range(n):
+    print(sets[i], end=" ")
