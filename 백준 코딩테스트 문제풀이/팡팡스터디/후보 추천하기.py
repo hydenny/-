bea87cb7688279b588b400892fd3f7ada46bc4a8
hayoung0402 <hy950402@gmail.com>
2021-12-28 13:46:58 +0900

    후보 추천하기

--- 백준 코딩테스트 문제풀이/팡팡스터디/후보 추천하기.py
+++ 백준 코딩테스트 문제풀이/팡팡스터디/후보 추천하기.py
@@ -1,11 +1,32 @@
 import sys
+from collections import deque
+
+
+def update(candidate):
+    if len(candidates) == n:
+        if candidate not in candidates.keys():
+            idx = list(candidates.values()).index(min(candidates.values()))
+            del candidates[list(candidates.keys())[idx]]
+        
+    if candidate not in candidates.keys():
+            candidates[candidate] = 1
+    else:
+        candidates[candidate] += 1
+    
+    return list(candidates.keys())
+
+
+def solution(n):
+    for _ in range(recommend):
+        candidate = number.popleft()
+        result = update(candidate)
+    result.sort()
+    for i in range(len(result)):
+        print(result[i], end=" ")
 
 n = int(input())
 recommend = int(input())
-number = list(map(int, input().split()))
+number = deque(map(int, input().split()))
+candidates = {}
 
-sets = set(number[-(n + 1):])
-sets = list(sets)
-sets.sort()
-for i in range(n):
-    print(sets[i], end=" ")
+solution(n)
