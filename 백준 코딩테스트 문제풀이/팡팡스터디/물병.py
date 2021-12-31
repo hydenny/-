47219167a38f39756bc53e84cee16b43a1cd4b95
hayoung0402 <hy950402@gmail.com>
2021-12-31 23:46:22 +0900

    물병

--- 백준 코딩테스트 문제풀이/팡팡스터디/물병.py
+++ 백준 코딩테스트 문제풀이/팡팡스터디/물병.py
@@ -2,9 +2,13 @@ import sys
 
 
 def solution(n, k):
-    
-    
-    return
+    answer = 0
+    print(bin(n))
+    while bin(n).count('1') > k:
+        plus = 2 ** (bin(n)[::-1].index('1'))
+        answer += plus
+        n += plus
+    print(answer)
 
 
 n, k = map(int, input().split())
