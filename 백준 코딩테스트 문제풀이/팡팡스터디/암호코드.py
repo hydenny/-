b755893dc9182ed2437391fb9e8dd37a4874a549
hayoung0402 <hy950402@gmail.com>
2022-01-06 22:59:21 +0900

    암호코드

--- 백준 코딩테스트 문제풀이/팡팡스터디/암호코드.py
+++ 백준 코딩테스트 문제풀이/팡팡스터디/암호코드.py
@@ -2,12 +2,24 @@ import sys
 
 
 def solution(code):
-    case = 0
+    length = len(code)
+    dp = [0 for _ in range(length+1)]
     
-    return case
+    if (code[0] == 0) :
+        print("0")
+    else :
+        code = [0] + code
+        dp[0] = dp[1] = 1
+        for i in range(2, length + 1):
+            if code[i] > 0:
+                dp[i] += dp[i - 1]
+            temp = code[i - 1] * 10 + code[i]
+            if temp >= 10 and temp <= 26 :
+                dp[i] += dp[i - 2]
+        print(dp[length] % 1000000)
 
 
 input = sys.stdin.readline
-code = int(input())
+code = list(map(int, input().rstrip()))
 
 solution(code)
