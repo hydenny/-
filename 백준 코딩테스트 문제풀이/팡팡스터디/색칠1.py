c0dcd9381c5cc2866b5aa920eb5af1dfecdddb79
hayoung0402 <hy950402@gmail.com>
2022-01-08 22:26:53 +0900

    색칠1

---
+++ 백준 코딩테스트 문제풀이/팡팡스터디/색칠1.py
@@ -0,0 +1,35 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def step1(board):
+    
+    return
+
+
+def step2(board):
+    
+    return
+
+
+def step3(board):
+    
+    return
+
+
+def step4(board):
+    
+    return
+
+
+def solution(board):
+    
+    return
+
+
+w, h, f, c, x1, y1, x2, y2 = map(int, input().split())
+
+board = [[False] * w for _ in range(h)]
+
+solution(board)
